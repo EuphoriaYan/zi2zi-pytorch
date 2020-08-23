9f0686957d64d043ddbd619dbd72dd6755011229
Euphoria Yan <ysq58000@foxmail.com>
2020-08-23 22:51:00 +0800

    update

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,7 +1,8 @@
 
+export CUDA_VISIBLE_DEVICES=2
 
 python train.py \
---experiment_dir experiment \
+--experiment_dir ZhongHuaSong2Shufa_experiment \
 --gpu_ids cuda:0 \
 --epoch 50 \
 --batch_size 64 \
