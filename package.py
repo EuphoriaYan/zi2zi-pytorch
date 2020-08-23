086233626e747e12529c3bda32f1d3e662f045f7
Euphoria Yan <ysq58000@foxmail.com>
2020-08-23 22:31:59 +0800

    update

--- package.py
+++ package.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import re
 
 
-def pickle_examples(paths, train_path, val_path, train_val_split=0.1):
+def pickle_examples_with_split_ratio(paths, train_path, val_path, train_val_split=0.1):
     """
     Compile a list of examples into pickled format, so during
     the training, all io will happen in memory
@@ -27,6 +27,16 @@ def pickle_examples(paths, train_path, val_path, train_val_split=0.1):
                         pickle.dump(example, ft)
 
 
+def pickle_examples_with_file_name(paths, obj_path):
+    with open(obj_path, 'wb') as fa:
+        for p in tqdm(paths):
+            label = int(os.path.basename(p).split("_")[0])
+            with open(p, 'rb') as f:
+                img_bytes = f.read()
+                example = (label, img_bytes)
+                pickle.dump(example, fa)
+
+
 parser = argparse.ArgumentParser(description='Compile list of images into a pickled object for training')
 parser.add_argument('--dir', required=True, help='path of examples')
 parser.add_argument('--save_dir', required=True, help='path to save pickled files')
@@ -34,6 +44,7 @@ parser.add_argument('--split_ratio', type=float, default=0.1, dest='split_ratio'
                     help='split ratio between train and val')
 parser.add_argument('--start_num', type=int, default=None)
 parser.add_argument('--end_num', type=int, default=None)
+parser.add_argument('--save_obj_dir', type=str, default=None)
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -55,5 +66,12 @@ if __name__ == "__main__":
             continue
         cur_file_list.append(file_name)
 
-    pickle_examples(cur_file_list, train_path=train_path, val_path=val_path,
-                    train_val_split=args.split_ratio)
+    if args.split_ratio == 0 and args.save_obj_dir is not None:
+        pickle_examples_with_file_name(cur_file_list, args.save_obj_dir)
+    else:
+        pickle_examples_with_split_ratio(
+            cur_file_list,
+            train_path=train_path,
+            val_path=val_path,
+            train_val_split=args.split_ratio
+        )
