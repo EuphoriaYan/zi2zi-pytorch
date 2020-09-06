ef2c2f0625766a82c456762791352c1d2c360ad9
Euphoria Yan <ysq58000@foxmail.com>
2020-09-06 21:46:30 +0800

    update

--- scripts/run_infer.sh
+++ scripts/run_infer.sh
@@ -3,7 +3,7 @@
 python infer.py \
 --experiment_dir massive_experiment1 \
 --embedding_num 205 \
---start_from 801 \
+--start_from 201 \
 --gpu_ids cuda:1 \
 --resume 240000 \
 --obj_path massive_experiment1/data/val.obj \
