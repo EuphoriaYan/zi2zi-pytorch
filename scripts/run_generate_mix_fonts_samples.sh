5076c76103ae50b3f5508aa094e463b508eb20fc
Euphoria Yan <ysq58000@foxmail.com>
2020-09-08 01:35:27 +0800

    update

--- scripts/run_generate_mix_fonts_samples.sh
+++ scripts/run_generate_mix_fonts_samples.sh
@@ -1,11 +1,12 @@
 
+export CUDA_VISIBLE_DEVICES=3
 
 python generate_mix_fonts_samples.py \
 --create_num 0 \
 --fonts_json /disks/sdb/projs/AncientBooks/data/chars/font_missing.json \
 --sample_dir fonts_samples/ \
 --experiment_dir massive_experiment0 \
---gpu_ids cuda:3 \
+--gpu_ids cuda \
 --embedding_num 205 \
 --resume 300000 \
 
