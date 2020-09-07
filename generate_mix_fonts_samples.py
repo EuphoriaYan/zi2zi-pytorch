0d132f274ab7f0fb5bd5f179b3cf0c20ea4a7286
Euphoria Yan <ysq58000@foxmail.com>
2020-09-08 01:48:11 +0800

    update

--- generate_mix_fonts_samples.py
+++ generate_mix_fonts_samples.py
@@ -209,6 +209,7 @@ class create_mix_ch_handle:
             input_img = draw_single_char(ch, self.fontPlane15, args.canvas_size, args.x_offset, args.y_offset)
         else:
             raise ValueError
+        input_img = input_img.convert('L')
         input_tensor = transforms.ToTensor()(input_img)
         input_tensor = transforms.Normalize(0.5, 0.5)(input_tensor).unsqueeze(0)
         label = torch.tensor(self.idx, dtype=torch.long).unsqueeze(0)
