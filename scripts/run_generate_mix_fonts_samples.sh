22513aeb59795c4c5fbc4d88953357db2934818b
Euphoria Yan <ysq58000@foxmail.com>
2020-09-08 01:34:52 +0800

    update

--- scripts/run_generate_mix_fonts_samples.sh
+++ scripts/run_generate_mix_fonts_samples.sh
@@ -5,7 +5,7 @@ python generate_mix_fonts_samples.py \
 --fonts_json /disks/sdb/projs/AncientBooks/data/chars/font_missing.json \
 --sample_dir fonts_samples/ \
 --experiment_dir massive_experiment0 \
---gpu_ids cuda:0 \
+--gpu_ids cuda:3 \
 --embedding_num 205 \
 --resume 300000 \
 
