ea45f1427e035e0953fb3d0fd21c8321e8c7ea26
Euphoria Yan <ysq58000@foxmail.com>
2020-09-14 00:56:08 +0800

    update

--- massive_font2img.py
+++ massive_font2img.py
@@ -12,15 +12,52 @@ import re
 from tqdm import tqdm
 import random
 from fontTools.ttLib import TTFont
+from torchvision import transforms
+from torch import nn
 
 
-def draw_single_char(ch, font, canvas_size, x_offset, y_offset):
-    img = Image.new("RGB", (canvas_size, canvas_size), (255, 255, 255))
+def draw_single_char(ch, font, canvas_size, x_offset=0, y_offset=0):
+    img = Image.new("L", (canvas_size * 2, canvas_size * 2), 0)
     draw = ImageDraw.Draw(img)
     try:
-        draw.text((x_offset, y_offset), ch, (0, 0, 0), font=font)
+        draw.text((10, 10), ch, 255, font=font)
     except OSError:
         return None
+    bbox = img.getbbox()
+    if bbox is None:
+        return None
+    l, u, r, d = bbox
+    l = max(0, l - 5)
+    u = max(0, u - 5)
+    r = min(canvas_size * 2 - 1, r + 5)
+    d = min(canvas_size * 2 - 1, d + 5)
+    if l >= r or u >= d:
+        return None
+    img = np.array(img)
+    img = img[u:d, l:r]
+    img = 255 - img
+    img = Image.fromarray(img)
+    # img.show()
+    width, height = img.size
+    # Convert PIL.Image to FloatTensor, scale from 0 to 1, 0 = black, 1 = white
+    try:
+        img = transforms.ToTensor()(img)
+    except SystemError:
+        return None
+    img = img.unsqueeze(0)  # 加轴
+    pad_len = int(abs(width - height) / 2)  # 预填充区域的大小
+    # 需要填充区域，如果宽大于高则上下填充，否则左右填充
+    if width > height:
+        fill_area = (0, 0, pad_len, pad_len)
+    else:
+        fill_area = (pad_len, pad_len, 0, 0)
+    # 填充像素常值
+    fill_value = 1
+    img = nn.ConstantPad2d(fill_area, fill_value)(img)
+    # img = nn.ZeroPad2d(m)(img) #直接填0
+    img = img.squeeze(0)  # 去轴
+    img = transforms.ToPILImage()(img)
+    img = img.resize((canvas_size, canvas_size), Image.ANTIALIAS)
     return img
 
 
@@ -139,5 +176,3 @@ if __name__ == "__main__":
                 if img is not None:
                     img.save(os.path.join(args.sample_dir, '%d_%05d.png' % (idx, cur)))
                     cur += 1
-    with open('font_label_map.json', 'w', encoding='utf-8') as fp:
-        json.dump(font_label_map, fp)
