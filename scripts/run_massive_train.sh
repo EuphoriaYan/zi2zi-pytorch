6c97c0d0f7e4aa775da6bb1ec5286f214b846819
Euphoria Yan <ysq58000@foxmail.com>
2020-09-15 00:24:11 +0800

    update

--- scripts/run_massive_train.sh
+++ scripts/run_massive_train.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=3
 
 python massive_train.py \
 --experiment_dir songhei_experiment \
---gpu_ids cuda:0 \
+--gpu_ids cuda \
 --embedding_num 250 \
 --epoch 20 \
 --batch_size 128 \
