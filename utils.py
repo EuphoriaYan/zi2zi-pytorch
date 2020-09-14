d6ce07df573e5d7e2930a5bd89df45d39264ee04
Euphoria Yan <ysq58000@foxmail.com>
2020-09-14 20:40:15 +0800

    update

---
+++ utils.py
@@ -0,0 +1,6 @@
+import re
+
+
+def natural_key(string_):
+    """See http://www.codinghorror.com/blog/archives/001018.html"""
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_.lower())]
