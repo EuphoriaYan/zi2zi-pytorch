d59cd9bf2f7f5d255bccd492a7e60d73138fb02e
Euphoria Yan <ysq58000@foxmail.com>
2020-09-29 22:52:55 +0800

    update

--- scripts/run_generate_mix_fonts_samples_v2.sh
+++ scripts/run_generate_mix_fonts_samples_v2.sh
@@ -2,10 +2,10 @@
 export CUDA_VISIBLE_DEVICES=2
 
 python generate_mix_fonts_samples_v2.py \
---fonts_json type/songhei_font_missing.json \
---sample_dir songhei_fonts_samples/ \
---bad_fonts charset/songhei_error_font.txt \
---experiment_dir songhei_experiment \
+--fonts_json type/caokai_font_missing.json \
+--sample_dir caokai_fonts_samples/ \
+--bad_fonts charset/caokai_error_font.txt \
+--experiment_dir caokai_experiment \
 --gpu_ids cuda \
 --embedding_num 250 \
---resume 74360 \
+--resume 46800 \
