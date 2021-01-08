de93f7d378099cc23dae1294e9dd6d36497d740b
Euphoria Yan <ysq58000@foxmail.com>
2021-01-09 05:20:18 +0800

    update

---
+++ scripts/run_category_train.sh
@@ -0,0 +1,10 @@
+
+export CUDA_VISIBLE_DEVICES=3
+
+python category.py \
+--action train \
+--resume fz2_experiment/checkpoint/187920_net_D.pth \
+--input_path fonts_output \
+--embedding_num 520 \
+--type_file type/方正第二批.txt \
+--epoch 30 \
